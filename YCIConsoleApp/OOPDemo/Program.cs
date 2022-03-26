22caec641dac091482a4b998a00e52a276d77144
mgkyaing <akog02@gmail.com>
2022-03-26 19:28:37 +0630

    add try-catch to get throw message from student.

--- YCIConsoleApp/OOPDemo/Program.cs
+++ YCIConsoleApp/OOPDemo/Program.cs
@@ -2,11 +2,11 @@
 
 namespace OOPDemo
 {
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Car MyCar=new Car(); //Create Car Object (it can called instance of Car Class)
+            Car MyCar = new Car(); //Create Car Object (it can called instance of Car Class)
             MyCar.Color = "Black";
             MyCar.LicenceNo = "YGN-66123";
             MyCar.Speed = "2000HP";
@@ -29,12 +29,12 @@ namespace OOPDemo
             Car c1 = new Car();
             c1.GetInfo();
 
-            Circle circle1= new Circle();//create object by using default constructor 
+            Circle circle1 = new Circle();//create object by using default constructor 
             circle1.Radius = 2.5;
             Console.WriteLine(circle1.GetArea());//
             Console.WriteLine(circle1.GetPrimeter());//
 
-            Circle circle2= new Circle(7.5);//craete object by using 1 parameter constructor
+            Circle circle2 = new Circle(7.5);//craete object by using 1 parameter constructor
             Console.WriteLine(circle2.GetArea());//
             Console.WriteLine(circle2.GetPrimeter());//
             circle2.SetRadius(50.5);
@@ -43,23 +43,32 @@ namespace OOPDemo
             Cat cat1 = new Cat();
             cat1.Sound();
             cat1.Sound(5);
-            
+
             Utilitity.TellMeTodayName();
             Utilitity.TellMeWhatTimeIsIt();
 
-            Student s1 = new Student();
-            s1.SetRollNo(1);
-            s1.SetName("su su");
-            s1.SetAge( 10);
-            s1.SetPhone("09256275319");
-            s1.GetInfo();
+            try
+            {
+                Student s1 = new Student();
+                s1.SetRollNo(1);
+                s1.SetName("su su");
+                s1.SetAge(10);
+                s1.SetEmail("susu@gmail.com");
+                s1.SetPhone("09256275319");
+                s1.GetInfo();
 
-            Student s2 = new Student();
-            s2.SetRollNo(-10);//Sensitive Data here 
-            s2.SetName("Khin Khin");
-            s2.SetAge(-10);//Sensitive Data here 
-            s2.SetPhone("09256275319123");
-            s2.GetInfo();
+                Student s2 = new Student();
+                s2.SetRollNo(-10);//Sensitive Data here 
+                s2.SetName("Khin Khin");
+                s2.SetEmail("khinkhin@gmail.com");
+                s2.SetAge(-10);//Sensitive Data here 
+                s2.SetPhone("09256275319123");
+                s2.GetInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error Because Of " + ex.Message);
+            }
 
             Console.WriteLine("Press any key to close this windows");
             Console.ReadKey();
--- YCIConsoleApp/OOPDemo/Student.cs
+++ YCIConsoleApp/OOPDemo/Student.cs
@@ -1,48 +1,59 @@
 ﻿using System;
-namespace OOPDemo{
-    public class Student{
+namespace OOPDemo
+{
+    public class Student
+    {
+
         private int RollNO;
         private string Name;
         private string Email;
         private string Phone;
         private int Age;
-        public void Register(string name){
+
+        public void Register(string name)
+        {
             Console.WriteLine(name);
         }
-        public void SetAge(int age){
+        public void SetAge(int age)
+        {
             if (age < 0)
                 throw new Exception("Invalid age.");
-            else 
-                Age= age;
+            else
+                Age = age;
         }
-        public void SetRollNo(int rollNo){
+        public void SetRollNo(int rollNo)
+        {
             if (rollNo <= 0)
                 throw new Exception("Invalid rollNo.");
             else
                 RollNO = rollNo;
         }
-        public void SetName(string name){
-            if (name==null || name=="")
+        public void SetName(string name)
+        {
+            if (name == null || name == "")
                 throw new Exception("Invalid Name.");
             else
-            Name = name;
+                Name = name;
         }
-        public void SetEmail(string email){
+        public void SetEmail(string email)
+        {
             if (!email.Contains("@"))
                 throw new Exception("Invalid Email.");
             else
-            Email = email;
+                Email = email;
         }
-        public void SetPhone(string phone){
-            if (phone.Length < 11 || phone.Length>11)
+        public void SetPhone(string phone)
+        {
+            if (phone.Length < 11 || phone.Length > 11)
                 throw new Exception("Invalid phone.");
             else
                 Phone = phone;
         }
-       public void GetInfo(){
+        public void GetInfo()
+        {
             Console.WriteLine("Student Info");
             Console.WriteLine("RollNo:" + RollNO);
-            Console.WriteLine("Name:"+Name);
+            Console.WriteLine("Name:" + Name);
             Console.WriteLine("Email:" + Email);
             Console.WriteLine("Phone:" + Phone);
         }
