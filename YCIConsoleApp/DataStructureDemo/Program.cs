4f05bb3fcbf912eb4d7cc48b86771424fba4ec08
mgkyaing <akog02@gmail.com>
2022-04-09 20:15:29 +0630

    module 6 )Chapter-19) Data Structure in C#.

---
+++ YCIConsoleApp/DataStructureDemo/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u1 = new User();
+            u1.Name = "user1";
+            u1.Email = "user1@yahoo.com";
+            u1.Password = "123456";
+            u1.DisplayUserInfo();
+
+            User u2 = new User()
+            {
+                Name = "user2",
+                Email = "user2@gmail.com",
+                Password = "123654856423"
+            };
+
+            int[] examMaks = new int[10];//0 to 9
+            examMaks[0] = 1;
+            List<int> items = new List<int>();
+            items.Add(1);
+            items.Add(10);
+            items.Add(200);
+            items.Add(300);
+            //items.Add("Hello");//Sematic Error 
+            foreach(int me in items)
+            {
+                Console.WriteLine(me);
+            }
+            //1
+            //10
+            //200
+            //300
+            IList<User> allUsers = new List<User>();
+            allUsers.Add(u1);
+            allUsers.Add(u2);
+            foreach(User u in allUsers)
+            {
+                u.DisplayUserInfo();
+            }
+            ArrayList allItems = new ArrayList();
+            allItems.Add("Hi");
+            allItems.Add(100);
+            allItems.Add(100.23);
+            allItems.Add(100);
+            allItems.Add("today is saturday");
+            for(int i = 0; i < allItems.Count; i++)
+            {
+                Console.WriteLine(allItems[i]);
+            }
+
+            try
+            {
+                Stack<int> data = new Stack<int>();
+                data.Push(1);
+                data.Push(2);
+                data.Push(3);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(data.Pop());
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);//stack is empty
+            }
+
+            try
+            {
+                Queue<string> sayGreeting = new Queue<string>();
+                sayGreeting.Enqueue("hi");//0
+                sayGreeting.Enqueue("hello");//1
+                sayGreeting.Enqueue("good evening");//2
+                int queueCount = sayGreeting.Count;
+                for(int i = 0; i <queueCount; i++)//sayGreeging wil be 3
+                {
+                    Console.WriteLine(sayGreeting.Dequeue());//
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);//Queue is empty
+            }
+
+            Console.WriteLine("press any key to close this window");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ YCIConsoleApp/DataStructureDemo/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace DataStructureDemo
+{
+  public  class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public void DisplayUserInfo()
+        {
+            Console.WriteLine("user information here");
+            Console.WriteLine("User Name :" + Name);
+            Console.WriteLine("User Email :" + Email);
+            Console.Write("User Password :");
+            ShowPasswordInStarFormat(Password.Length);//Method Invoke 
+        }
+        //Method Defination 
+        private void ShowPasswordInStarFormat(int passwordCount)
+        {
+            for(int i = 1; i <= passwordCount; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
